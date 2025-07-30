26b6d7f569429a9756cb91307e56969ab6862c16
Feyitee <anjuwontobi99@gmail.com>
2025-07-30 15:56:44 +0100

    styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,16 @@
 const Main = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <main>
+      <form>
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+          className="add-ingredient-form"
+        />
+        <button>Add Ingredient</button>
+      </form>
+    </main>
   );
 };
 
