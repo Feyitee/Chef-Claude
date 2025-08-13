b3b4605756e4a384e3faa146864c5d1e04734dc2
Feyitee <anjuwontobi99@gmail.com>
2025-08-13 14:30:24 +0100

    ingredientlistclauderecipecomponent

--- src/components/She.jsx
+++ src/components/She.jsx
@@ -123,20 +123,104 @@
 //   )
 // }
 
-import React from "react";
+// import React from "react";
+
+// const She = () => {
+//   const diaetaryData = FormData.getAll(dietaryRestrictions);
+//   const allData = {
+//     ...data,
+//     diaetaryData,
+//   };
+//   return (
+//     <div>
+//       <label htmlFor="description">Description:</label>
+//       <textarea id="description" name="description"></textarea>
+//     </div>
+//   );
+// };
+
+// export default She;
+
+// // Ternary instead of the And operator
+//         <div>
+//             {props.setup && <h3>{props.setup}</h3>}
+//             {isShown ? <p>{props.punchline}</p> : null}
+//             <button onClick={toggleShown}>{isShown ? "Hide" : "Show"} punchline</button>
+//             <hr />
+//         </div>
+
+// import React from 'react'
+
+import React from 'react'
+import padsData from "./pads"
+import Pad from "./Pad"
 
 const She = () => {
-  const diaetaryData = FormData.getAll(dietaryRestrictions);
-  const allData = {
-    ...data,
-    diaetaryData,
-  };
+
+  const [pads, setPads] = React.useState(padsData)
+  const buttonElements = pads.map((pad)=>{
+    return <Pad key ={pad.id}/>
+  })
   return (
     <div>
-      <label htmlFor="description">Description:</label>
-      <textarea id="description" name="description"></textarea>
+      
     </div>
-  );
-};
+  )
+}
 
-export default She;
+export default She
+
+export default function Pad(props) {
+
+  const [onoff, setoneoff] = React.useState(props.on)
+
+  function onn (){
+   setoneoff(prevon => !prevon)
+    }
+    
+    return (
+        <button style={{backgroundColor: props.color}} onClick ={onn}></button>
+    )
+}
+
+
+import React from "react"
+import padsData from "./pads"
+import Pad from "./Pad"
+
+export default function App() {
+    const [pads, setPads] = React.useState(padsData)
+    
+    function toggle(id) {
+      
+        console.log(id)
+        /**
+         * Challenge:
+         * Call setPads to update the state of the one pad that was
+         * clicked. Map over the previous pads array, and if the current
+         * item you're iterating over has the same id as the `id` passed
+         * to this function, then return a new object with the `on` value
+         * set to the opposite of what it was before.
+         * Otherwise (if the ids don't match), just return the previous
+         * item as it was, unchanged.
+         */
+    }
+    
+    const toggle(id) => {
+     setPads( prevPad => prevPad.map((item)=>{
+      item.id === id ? !item.on :{...item}
+      }))
+    }
+
+    const buttonElements = pads.map(pad => (
+        <Pad toggle={toggle} id={pad.id} key={pad.id} color={pad.color} on={pad.on}/>
+    ))
+    
+    return (
+        <main>
+            <div className="pad-container">
+                {buttonElements}
+            </div>
+        </main>
+    )
+}
