6651ca32b6a6c27cddf96d7bc3fc48c2ac97bf0a
Feyitee <anjuwontobi99@gmail.com>
2025-08-14 12:06:55 +0100

    thirdpartymarkdown

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
 
 const ClaudeRecipe = (props) => {
-  return <div>{props.recipe}</div>;
+  return (
+    <div>
+      <ReactMarkdown>{props.recipe}</ReactMarkdown>
+    </div>
+  );
 };
 
 export default ClaudeRecipe;
